3bda1e952a731472612f99d332d651413ced86e9
IamWangYunKai <615349118@163.com>
2020-01-29 19:35:57 +0800

    send action in json

---
+++ send_action.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import json
+import socket
+from time import sleep
+
+port = 23333
+address = '233.233.233.233'
+
+def sendData():
+    data = {'v':15, 'w':2.5, 'c':True}
+    print('send data:', data)
+    data = json.dumps(data).encode()
+    ret = clisocket.sendto(data,(address,port))
+    return ret
+
+if __name__ == '__main__':
+    clisocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    clisocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+    while True:
+        sendData()
+        sleep(0.5)
